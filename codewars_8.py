f2cc97acf5e5fd1a38f9f34afcb0409e35ae66a9
Bruce Chung <brucekchung@gmail.com>
2018-07-06 10:18:06 -0600

    Add new kebabize exercise

---
+++ codewars_8.py
@@ -0,0 +1,8 @@
+# Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+
+def kebabize(string):
+    return string
+
+print('result: ', kebabize('camelCaseJavaScriptStringThing'))
+
