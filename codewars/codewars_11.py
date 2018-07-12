dd1c9377f7d14cd05feb02955dc8c0118dde6b0f
Bruce Chung <brucekchung@gmail.com>
2018-07-12 10:38:11 -0600

    Add new exercise

--- codewars/codewars_11.py
+++ codewars/codewars_11.py
@@ -13,3 +13,4 @@ print('abc: ', solution('abc'))
 # import re
 # def solution(s):
 #     return re.findall(".{2}", s + "_")
+
---
+++ codewars/codewars_12.py
@@ -0,0 +1,8 @@
+# We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+# So given a string "super", we should return a list of [2, 4].
+
+def vowel_indices(word):
+    pass
+
+print('vowel index: ', vowel_indices('hello'))
