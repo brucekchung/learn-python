cbaa09973e090db12e50b27074f17fa502670a99
Bruce Chung <brucekchung@gmail.com>
2018-07-16 10:13:38 -0600

    Add new cw exercise

---
+++ codewars/codewars_13.py
@@ -0,0 +1,6 @@
+# In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+def filter_list(l):
+    pass
+
+print('filter: ', filter_list([1,2,'a','b']))
