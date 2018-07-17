c62ee8e302be89171ddab5d6aa42c6f8f975a30d
Bruce Chung <brucekchung@gmail.com>
2018-07-17 10:39:03 -0600

    Add solution

--- codewars/codewars_13.py
+++ codewars/codewars_13.py
@@ -1,6 +1,6 @@
 # In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
 def filter_list(l):
-    pass
+    return [i for i in l if not isinstance(i, str)]
 
 print('filter: ', filter_list([1,2,'a','b']))
