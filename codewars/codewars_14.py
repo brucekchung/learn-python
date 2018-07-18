203c3a8059884b3301494f11d7f359bc4526b610
Bruce Chung <brucekchung@gmail.com>
2018-07-18 10:41:40 -0600

    Add roman-integer converter variation

---
+++ codewars/codewars_14.py
@@ -0,0 +1,7 @@
+# Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+
+def solution(roman):
+    pass
+
+print('MXVII ', solution('MXVII'))
