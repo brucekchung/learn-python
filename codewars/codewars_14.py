d299715c177b3b45ab64098645a9b398c3cb746d
Bruce Chung <brucekchung@gmail.com>
2018-07-19 09:33:37 -0600

    Add codewars solution to roman converter

--- codewars/codewars_14.py
+++ codewars/codewars_14.py
@@ -2,6 +2,26 @@
 
 
 def solution(roman):
-    pass
+    dict = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1
+    }
+
+    last, total = 0, 0
+    for c in list(roman)[::-1]:
+        if last == 0:
+            total += dict[c]
+        elif last > dict[c]:
+            total -= dict[c]
+        else:
+            total += dict[c]
+        last = dict[c]
+    return total
+
 
 print('MXVII ', solution('MXVII'))
