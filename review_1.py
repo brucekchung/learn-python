a9f967ae4268315d194baa409ead235dda742e2d
Bruce Chung <brucekchung@gmail.com>
2018-07-27 12:38:52 -0600

    Add review exercises

---
+++ review_1.py
@@ -0,0 +1,8 @@
+#1) return total time needed to queue given the customers and tills
+
+
+
+#2) Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+
+
