b6c2860d13d4935226a6ea3916e7f0163b25eca2
Bruce Chung <brucekchung@gmail.com>
2018-07-31 10:36:56 -0600

    Begin review ex

--- review_1.py
+++ review_1.py
@@ -1,6 +1,12 @@
 #1) return total time needed to queue given the customers and tills
 
 
+def total_time(customers, tills):
+    pass
+
+
+print('time: ', total_time(1, 1))
+
 
 #2) Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
